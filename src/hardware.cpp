9bbeefee9347f8d8998c6fea56c4398e324eac0d
TEC-Andres <a01287002@tec.mx>
2026-09-22 07:59:57 -0600

    Initial commit

---
+++ src/hardware.cpp
@@ -0,0 +1,11 @@
+/*
+#   Activity 1.4 - Search Algorithms
+#   Andrés Rodríguez Cantú 
+#   
+#   Copyright (C) Tecnólogico de Monterrey
+#   
+#   File: platformio.ini
+#   
+#   Created:                   22/09/2026
+#   Last Modified:             22/09/2026
+*/
