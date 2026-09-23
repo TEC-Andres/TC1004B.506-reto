cd690aff3b053507c34c2237bdfa69a835acd167
Andrés Rodríguez Cantú <a01287002@tec.mx>
2026-09-22 19:19:15 -0600

    Initial platformio endpoint (#6)

--- firmware/src/hardware.cpp
+++ firmware/src/hardware.cpp
@@ -9,3 +9,14 @@
 #   Created:                   22/09/2026
 #   Last Modified:             22/09/2026
 */
+#include <Arduino.h>
+
+void setup() {
+    Serial.begin(115200);
+    delay(5000);
+    Serial.println("Hello, World!");
+}
+
+void loop() {
+    // Your code here
+}
